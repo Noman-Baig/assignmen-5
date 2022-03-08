e128298ec8466b5d28c5993bb1b25a38381cc04d
Noman-Baig <mirzanomanbaig449@gmail.com>
2022-03-08 19:26:08 +0500

    first commitassignment5

---
+++ main.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+void main(){
+// print("\nTask 1\n");
+// List str = ["Hello","Zabtech","welcome", "to","the","Tech","World"];
+// print(str);
+// print("\nTask 2\n");
+// List mix = ["One","Two","Three",4,5,4.5,true,false];
+// print(mix);
+// print("\nTask 3\n");
+// List edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS","M.Phil", "PhD"];
+// print(edu);
+// print("\nTask 4\n");
+// List stu = ["noman","mujiz","ghulam"];
+// print(stu);
+// int totalmarks = 500;
+// stdout.write("Enter Your Name : ");
+// String stName = stdin.readLineSync()!;
+// if(stName == "noman"||stName == "ghulam"||stName == "mujiz"){
+//     stdout.write("Enter Obtained Marks : ");
+// int obt = int.parse(stdin.readLineSync()!);
+// num per = (obt /500 )*100;
+// print (per);
+// }else{
+//     print("Student Not Enrolled");
+// }
+// print("\nTask 5\n");
+// List stuScore = [434,321,430,530];
+// print(stuScore);
+// stuScore.sort();
+// print ("Asending : ${stuScore}");
+// print("\nTask 6\n");
+// print ("Desending : ${stuScore.reversed.toList()}");
+// print("\nTask 7\n");
+// List pLang=["Python","Java","c++","c","Dart"];
+// var ind = pLang.getRange(2,4).toList();
+// print("Inde for 2 to 3 : ${ind}");
+// print("\nTask 8\n");
+// List clr = ["black","Blue","red","purple","yellow"];
+// print(clr);
+// stdout.write("What Color You Want to add in Start : ");
+// String color = stdin.readLineSync()!;
+// clr.insert(0,color);
+// print(clr);
+// stdout.write("What Color You Want to add in End : ");
+// String colorEnd = stdin.readLineSync()!;
+// clr.add(colorEnd);
+// print(clr);
+// stdout.write("Add two or more colors in Start : ");
+// String colorplus = stdin.readLineSync()!;
+// var sp = colorplus.split(" ");
+// clr.insertAll(0,sp);
+// print(clr);
+// stdout.write("Delete Colors 1 for from Start and 2 for End : ");
+// int dlt = int.parse(stdin.readLineSync()!);
+// if (dlt == 1){
+//     clr.removeAt(0);
+//     print(clr);
+
+// }else if(dlt == 2){
+//     clr.removeAt(clr.length-1);
+//     print(clr);
+
+// }else{
+//     print("Choose 1 or 2");
+// }
+// print("Add color on Your Specific index");
+// stdout.write("Enter Index Number : ");
+// int indNum = int.parse(stdin.readLineSync()!);
+// stdout.write("What color You want to Add : ");
+// String addedClr = stdin.readLineSync()!;
+// clr.insert(indNum, addedClr);
+// print(clr);
+// print("First Element : '${clr[0]}' Last Element : '${clr.last}'");
+// print("\nTask 9\n");
+// List cities = ["karachi","hyderabad","Sargodha","Islamabad"];
+// print(cities);
+// var got = cities.getRange(1,3).toList();
+// print(got);
+// print("\nTask 10\n");
+// List catLine = ["This","is","my","cat"];
+// print("In List : ${catLine} Type : ${catLine.runtimeType}");
+// var inString = catLine.toString();
+// var res = inString.replaceAll(","," ");
+// print ("Chnaged in String : ${res} Type : ${res.runtimeType}");
+// print("\nTask 11\n");
+// print("Password Must have 6 Charecters \nand not number in Start \nAnd in End Must Capital Leter Or Number");
+// stdout.write("Enter Password : ");
+// var pass = stdin.readLineSync()!;
+
+// if(pass.length < 6){
+//   print("Length Must be Greater Than 6");
+
+// }
+// else if(pass.codeUnitAt(0)>=48 && pass.codeUnitAt(0)<=57){
+//   print("Not Enter Number In Start");
+// }
+// else if (pass.codeUnitAt(pass.length-1)>=65 && pass.codeUnitAt(pass.length-1) <= 90 || pass.codeUnitAt(pass.length-1)>=48 && pass.codeUnitAt(pass.length-1)<=57 ){
+//   print(pass);
+// }
+// else{
+//   print("Please Enter Capital Letetr Or Number In End");
+// }
+
+
+}
